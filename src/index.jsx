0e86da010a9aa48e9296d24cbca93139487fd232
Mariano <creative@Mini-de-Mariano.fibertel.com.ar>
2023-04-15 14:03:02 -0300

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {createHashRouter, RouterProvider, HashRouter} from 'react-router-dom';
 
 import { JulietArtsApp } from './JulietArtsApp';
 import './styles.css';
 
+const router = createHashRouter([
+    {
+        path: "/*",
+        element: <JulietArtsApp />,
+    }
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JulietArtsApp />
-    </BrowserRouter>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
